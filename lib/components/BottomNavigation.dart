8ad4a6bd4a0f761d2b8b82e474cf64c1da38ce46
Iftekar Hossain <iftekar.hossain123@gmail.com>
2026-09-06 19:59:18 -0400

    Adding styling to the bottom navigation

--- lib/components/BottomNavigation.dart
+++ lib/components/BottomNavigation.dart
@@ -25,22 +25,35 @@ class _BottomNavigationState extends State<BottomNavigation> {
     return Scaffold(
       body: IndexedStack(index: _selectedPage, children: _pages),
 
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedPage,
-        onTap: _onItemTapped,
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.grey,
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(color: Colors.grey.shade300, width: 1.0),
+          ),
+        ),
 
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.video_camera_front_outlined, size: 35),
-            label: 'Record',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.description_outlined, size: 35),
-            label: 'Script',
-          ),
-        ],
+        child: BottomNavigationBar(
+          elevation: 8,
+          backgroundColor: Colors.white,
+          currentIndex: _selectedPage,
+          onTap: _onItemTapped,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.grey.shade400,
+
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.video_camera_front_outlined, size: 30),
+              activeIcon: Icon(Icons.video_camera_front, size: 30),
+              label: 'Record',
+            ),
+
+            BottomNavigationBarItem(
+              icon: Icon(Icons.description_outlined, size: 30),
+              activeIcon: Icon(Icons.description, size: 30),
+              label: 'Script',
+            ),
+          ],
+        ),
       ),
     );
   }
