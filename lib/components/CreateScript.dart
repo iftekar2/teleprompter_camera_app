87404f0d51a56beda50e678aefcb9505853aafb9
Iftekar Hossain <iftekar.hossain123@gmail.com>
2026-09-07 11:57:04 -0400

    Creating a dedicated page to create script

---
+++ lib/components/CreateScript.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Createscript extends StatelessWidget {
+  const Createscript({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leadingWidth: 120,
+        leading: InkWell(
+          onTap: () => Navigator.maybePop(context),
+          child: const Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(width: 20),
+              Icon(Icons.arrow_back, color: Colors.black),
+              SizedBox(width: 4),
+              Text(
+                'Scripts',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+
+      body: Center(child: Text("Create Script Page")),
+    );
+  }
+}
