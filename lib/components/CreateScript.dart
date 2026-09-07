c14cfe0df449092e70e6ed42c1c0ac36a6c373cd
Iftekar Hossain <iftekar.hossain123@gmail.com>
2026-09-07 14:42:04 -0400

    Title and Description box looks good

--- lib/components/CreateScript.dart
+++ lib/components/CreateScript.dart
@@ -30,7 +30,72 @@ class Createscript extends StatelessWidget {
         ),
       ),
 
-      body: Center(child: Text("You can create a new Script here")),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter Title of Script",
+                  hintStyle: TextStyle(
+                    color: Colors.grey.shade400,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+
+              SizedBox(height: 30),
+              SizedBox(
+                child: TextField(
+                  maxLines: 16,
+                  decoration: InputDecoration(
+                    hintText: "Type or paste your script here...",
+                    hintStyle: const TextStyle(
+                      color: Colors.grey,
+                      fontSize: 20,
+                    ),
+
+                    contentPadding: const EdgeInsets.only(
+                      left: 24.0,
+                      top: 12.0,
+                      right: 16.0,
+                      bottom: 16.0,
+                    ),
+
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(24.0),
+                      borderSide: const BorderSide(color: Colors.grey),
+                    ),
+
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(24.0),
+                      borderSide: const BorderSide(
+                        color: Colors.black,
+                        width: 2.0,
+                      ),
+                    ),
+
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(24.0),
+                    ),
+                  ),
+
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
